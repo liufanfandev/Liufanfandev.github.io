3226c0cd0ec76f0c267ef812f5e6d481eecacf8c
liufanfandev <liufanfandev@163.com>
2019-07-03 16:46:25 +0800

    调整模板

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,95 +1,34 @@
-/*
-	Future Imperfect by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+function toggleMenu() {
+  var nav = document.getElementsByClassName("site-header-nav")[0];
+  if (nav.style.display == "inline-flex") {
+    nav.style.display = "none";
+  } else {
+    nav.style.display = "inline-flex";
+  }
+}
 
-(function($) {
+jQuery(function() {
+  // 回到顶部
+  function toTop () {
+    var $toTop = $(".gotop");
 
-	var	$window = $(window),
-		$body = $('body'),
-		$menu = $('#menu'),
-		$sidebar = $('#sidebar'),
-		$main = $('#main');
+    $(window).on("scroll", function () {
+      if ($(window).scrollTop() >= $(window).height()) {
+        $toTop.css("display", "block").fadeIn();
+      } else {
+        $toTop.fadeOut();
+      }
+    });
 
-	// Breakpoints.
-		breakpoints({
-			xlarge:   [ '1281px',  '1680px' ],
-			large:    [ '981px',   '1280px' ],
-			medium:   [ '737px',   '980px'  ],
-			small:    [ '481px',   '736px'  ],
-			xsmall:   [ null,      '480px'  ]
-		});
+    $toTop.on("click", function (evt) {
+      var $obj = $("body,html");
+      $obj.animate({
+        scrollTop: 0
+      }, 240);
 
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+      evt.preventDefault();
+    });
+  }
 
-	// Menu.
-		$menu
-			.appendTo($body)
-			.panel({
-				delay: 500,
-				hideOnClick: true,
-				hideOnSwipe: true,
-				resetScroll: true,
-				resetForms: true,
-				side: 'right',
-				target: $body,
-				visibleClass: 'is-menu-visible'
-			});
-
-	// Search (header).
-		var $search = $('#search'),
-			$search_input = $search.find('input');
-
-		$body
-			.on('click', '[href="#search"]', function(event) {
-
-				event.preventDefault();
-
-				// Not visible?
-					if (!$search.hasClass('visible')) {
-
-						// Reset form.
-							$search[0].reset();
-
-						// Show.
-							$search.addClass('visible');
-
-						// Focus input.
-							$search_input.focus();
-
-					}
-
-			});
-
-		$search_input
-			.on('keydown', function(event) {
-
-				if (event.keyCode == 27)
-					$search_input.blur();
-
-			})
-			.on('blur', function() {
-				window.setTimeout(function() {
-					$search.removeClass('visible');
-				}, 100);
-			});
-
-	// Intro.
-		var $intro = $('#intro');
-
-		// Move to main on <=large, back to sidebar on >large.
-			breakpoints.on('<=large', function() {
-				$intro.prependTo($main);
-			});
-
-			breakpoints.on('>large', function() {
-				$intro.prependTo($sidebar);
-			});
-
-})(jQuery);
+  toTop();
+});
